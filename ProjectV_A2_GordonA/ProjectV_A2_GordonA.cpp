9019b6211e5089f765591ef3b002ccd18c40f82f
Andrew Gordon <65138166+andrewgg1@users.noreply.github.com>
2024-09-23 20:42:03 -0400

    Step 3: Student Data Parsing implementation
    - Student Data file containing just names is parsed into a vector

--- ProjectV_A2_GordonA/ProjectV_A2_GordonA.cpp
+++ ProjectV_A2_GordonA/ProjectV_A2_GordonA.cpp
@@ -2,8 +2,48 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// STUDENT_DATA struct to contain data of each student
+struct STUDENT_DATA {
+    string firstName;
+    string lastName;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // vector to store student data
+    vector<STUDENT_DATA> studentData;
+    string studentDataLine;
+
+    // open the student data file
+    ifstream studentDataFile("StudentData.txt");
+    if (!studentDataFile.is_open()) {
+        cerr << "Failed to open the Student Data file." << endl;
+        return 1;
+    }
+
+    while (getline(studentDataFile, studentDataLine)) {
+        // find the index of the comma
+        size_t commaIndex = studentDataLine.find(',');
+        if (commaIndex != string::npos) {
+            // create new STUDENT_DATA struct
+            STUDENT_DATA newStudent;
+
+            // get the first and last names, save to struct
+            newStudent.lastName = studentDataLine.substr(0, commaIndex);;
+            newStudent.firstName = studentDataLine.substr(commaIndex + 2); // +2 to skip ", "
+
+            // push the student struct into the vector of students
+            studentData.push_back(newStudent);
+        }
+    }
+
+    // close the student data file
+    studentDataFile.close();
+
+    return 0;
 }
